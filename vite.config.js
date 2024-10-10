73216c9bcf68acdc8d45663319f1fcddf64017ed
Tasneem1224 <tasneemhossameldeen@gmail.com>
2024-10-10 02:35:57 -0800

    change vite size

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/MyPortfolio/',  
-  plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
 
+  base: "/MyPortfolio/",
+  plugins: [react()],
+});
