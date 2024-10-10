a04bd0f79e973f6c3ba044ea39780e1a89387d18
Tasneem1224 <tasneemhossameldeen@gmail.com>
2024-10-10 02:54:14 -0800

    changes to deploy

---
+++ src/components/pages/alldesigns.js
@@ -0,0 +1,102 @@
+const allDesignsData = [
+  {
+    id: 1,
+    title: "Food",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/14.jpg",
+  },
+  {
+    id: 2,
+    title: "Sports",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/19.jpg",
+  },
+  {
+    id: 3,
+    title: "Home",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/18.jpg",
+  },
+  {
+    id: 4,
+    title: "Store",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/21.jpg",
+  },
+  {
+    id: 5,
+    title: "Food",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/12.jpg",
+  },
+  {
+    id: 6,
+    title: "Food",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/11.jpg",
+  },
+
+  {
+    id: 7,
+    title: "Public Event",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/17.jpg",
+  },
+
+  {
+    id: 8,
+    title: "Sports",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/16.jpg",
+  },
+  {
+    id: 9,
+    title: "Food",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/110.jpg",
+  },
+  {
+    id: 10,
+    title: "Store",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/22.jpg",
+  },
+  {
+    id: 11,
+    title: "Social Media",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/15.jpg",
+  },
+  {
+    id: 12,
+    title: "Social Media",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/13.jpg",
+  },
+  {
+    id: 13,
+    title: "T-Shirt",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/33.jpg",
+  },
+  {
+    id: 14,
+    title: "Food",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/115.jpg",
+  },
+  {
+    id: 15,
+    title: "Social Media",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/111.jpg",
+  },
+  {
+    id: 16,
+    title: "Personal Card",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/34.jpg",
+  },
+];
+
+export default allDesignsData;
---
+++ src/components/pages/logos.js
@@ -0,0 +1,40 @@
+const logosData = [
+  {
+    id: 1,
+    title: "Store",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/21.jpg",
+  },
+  {
+    id: 2,
+    title: "Store",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/22.jpg",
+  },
+  {
+    id: 3,
+    title: "University",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/23.png",
+  },
+  {
+    id: 4,
+    title: "University",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/24.png",
+  },
+  {
+    id: 5,
+    title: "Company",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/25.png",
+  },
+  {
+    id: 6,
+    title: "Handmade",
+    description: "This is a sample description for Design",
+    imgSrc: "./images/26.jpg",
+  },
+];
+
+export default logosData;
